0c2584769de73c9de9420365a9d9b3a4c05fa64b
sasidhar <sasidharreddy488@gmail.com>
2026-04-21 00:15:05 +0530

    Updated UI and fixed

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT ?? 4173),
     allowedHosts: ['parkpoint2.onrender.com']
   }
 })
